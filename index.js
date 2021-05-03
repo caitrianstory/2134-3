1178d8dee2f6096ca85793e242adea9dfe94bfa8
Caitrian Story <you@example.com>
2021-05-03 11:14:04 -0700

    Add drive feature

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/drive', (req, res) => {
+	 res.setHeader("Access-Control-Allow-Origin", "*");
+	 console.log(req.query,req.body);
+  res.send("i'm driving");
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://math.seattleacademy.org:${1514}`)
+  console.log(`Example app listening at http://math.seattleacademy.org:${port}`)
 })
